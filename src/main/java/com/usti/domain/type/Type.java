0e5ab5279c02c218505b5413fd91dabd2f70956b
congcoding <congcoding@gmail.com>
2021-01-04 00:51:59 +0900

    [FEAT] type 테이블 description 컬럼 길이 수정

--- src/main/java/com/usti/domain/type/Type.java
+++ src/main/java/com/usti/domain/type/Type.java
@@ -17,6 +17,6 @@ public class Type {
     @Column(length = 20, nullable = false)
     private String name;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 500, nullable = false)
     private String description;
 }
